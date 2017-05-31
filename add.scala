9d85c6563257a1346da4d3992e6faca1312bbd7f
wangpingqian <15868135507@163.com>
2017-05-31 11:00:11 +0800

    second

---
+++ add.scala
@@ -0,0 +1,8 @@
+object add{
+   def addInt( a:Int, b:Int ) : Int = {
+      var sum:Int = 0
+      sum = a + b
+
+      return sum
+   }
+}
---
+++ array.scala
@@ -0,0 +1,23 @@
+import Array._
+
+object Test {
+   def main(args: Array[String]) {
+      var myMatrix = ofDim[Int](3,3)
+      
+      // 创建矩阵
+      for (i <- 0 to 2) {
+         for ( j <- 0 to 2) {
+            myMatrix(i)(j) = j;
+         }
+      }
+      
+      // 打印二维阵列
+      for (i <- 0 to 2) {
+         for ( j <- 0 to 2) {
+            print(" " + myMatrix(i)(j));
+         }
+         println();
+      }
+    
+   }
+}
